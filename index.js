59c8b0aa4916b4e30aef3c41064700ae988542ff
Daniel Schmidt <d.schmidt20@yahoo.com>
2021-11-23 09:56:55 -0800

    commit tree 27e8258195b28218c5ca9556a3e9547247c34966

---
+++ index.js
@@ -0,0 +1,12 @@
+// const divide = function(num1,num2){
+//         return num1 / num2
+// }
+// divide(2000, 100)
+
+const square = num3 => Math.pow(num3, num3)
+
+
+const add = (num4, num5) => num4 + num5
+add(7, 11)
+
+const divide = (num1=2000, num2=100) => num1 / num2
